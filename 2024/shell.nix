c366e89cef8e62cfd9b4af6e060b1f817cc0cfbc
Asmund Berge <asmund@berge.id>
2024-11-25 19:40:10 +0100

    2024 setup

---
+++ 2024/shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    gleam
+    glas
+    erlang
+  ];
+
+  shellHook = ''
+    LOCALE_ARCHIVE="$(nix-build --no-out-link '<nixpkgs>' -A glibcLocales)/lib/locale/locale-archive"
+
+    export LANG=en_GB.UTF-8
+  '';
+}
